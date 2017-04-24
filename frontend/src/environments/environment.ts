239de6f300b60c6cfe245aa874c7a28e7e059a3d
Nazar Hnatyshen <snoowker@gmail.com>
2017-04-24 11:53:01 +0300

    fix env

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
     production: false,
-    host: 'http://muzify.elantixtech.com',
+    host: 'http://beta.muzify.elantixtech.com',
     uploadTrackList: '/upload',
     uploadFilesUrl: '/api/uploadfiles/',
     musicList: '/music',
