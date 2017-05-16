ea3ec1aaace89b6c5b7e59d554d20cd053688b61
Nazar Hnatyshen <snoowker@gmail.com>
2017-05-16 16:59:50 +0300

    cs + waveform fixes

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -1,111 +1,104 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php if (!defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
 
-class Home extends MY_Controller {
+class Home extends MY_Controller
+{
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->model('browse_recommended');
+        $this->load->model('commonfn');
+        $this->load->model('home_modal');
 
-	function __construct()
-	{
-		parent::__construct();
-		$this->load->model('browse_recommended');
-		$this->load->model('commonfn');
-		$this->load->model('home_modal');
+        $this->rec_to_dis = 10;
+    }
 
-		$this->rec_to_dis = 10;
-	}
+    function index($action = null)
+    {
+        $this->load->helper('url');
+        $cm = $this->config->item('meta_keyword') . ',def,';
+        $this->config->set_item('meta_keyword', $cm);
+        if ($action == "music") {
+            $records = $this->browse_recommended->fetch_new_tracks("", $this->rec_to_dis, null, null, null, 373, 373, '', '', 38, 38);
+            $temp_name = "home/music_row.html";
+            $music_active = true;
+            $this->config->set_item('title', 'Music');
+        } else if ($action == "articles") {
+            $this->config->set_item('title', 'Articles');
+        } else if ($action == "news") {
+            $this->config->set_item('title', 'News');
+        } else if ($action == "instrumental") {
+            $records = $this->browse_recommended->fetch_instrumental("", $this->rec_to_dis, null, null, null, 373, 373, '', 38, 38);
+            $temp_name = "home/music_row.html";
+            $instrument_active = true;
+            $this->config->set_item('title', 'Instrumental');
+        } else if ($action == "license") {
+            $records = $this->browse_recommended->fetch_new_tracks("tt.license='y'", $this->rec_to_dis, null, null, null, 373, 373, '', '', 38, 38);
+            $temp_name = "home/music_row.html";
+            $license_active = true;
+            $this->config->set_item('title', 'Licence');
+        } else {
+            //default home url
+            $loadmore = "true";
+            $load_url = "home/";
+            $load_cont = "#all-items-masonary";
+            $load_tmpl = "home_article_row,home_music_row";
 
-	function index($action = NULL)
-	{
-		$this->load->helper('url');
-		$cm=$this->config->item('meta_keyword').',def,';
-		$this->config->set_item('meta_keyword',$cm);
-		if($action == "music")
-		{
-			$records = $this->browse_recommended->fetch_new_tracks("",$this->rec_to_dis,NULL,NULL,NULL,373,373,'','',38,38);	
-			$temp_name = "home/music_row.html";
-			$music_active = true;	
-			$this->config->set_item('title','Music');				
-		}
-		else if($action == "articles")
-		{
-			$this->config->set_item('title','Articles');
-		}
-		else if($action == "news")
-		{
-			$this->config->set_item('title','News');
-		}
-		else if($action == "instrumental")
-		{
-			$records = $this->browse_recommended->fetch_instrumental("",$this->rec_to_dis,NULL,NULL,NULL,373,373,'',38,38);	
-			$temp_name = "home/music_row.html";
-			$instrument_active = true;
-			$this->config->set_item('title','Instrumental');
-		}
-		else if($action == "license")
-		{
-			$records = $this->browse_recommended->fetch_new_tracks("tt.license='y'",$this->rec_to_dis,NULL,NULL,NULL,373,373,'','',38,38);	
-			$temp_name = "home/music_row.html";
-			$license_active = true;
-			$this->config->set_item('title','Licence');
-		}
-		else
-		{
-                    //default home url
-			$loadmore = "true";
-			$load_url = "home/";
-			$load_cont = "#all-items-masonary";
-			$load_tmpl = "home_article_row,home_music_row";
+            /*$records = $this->browse_recommended->fetch_new_tracks("",$this->rec_to_dis,NULL,NULL,NULL,373,373,'','',38,38);	*/
+            $records = $this->home_modal->get_overview("", $this->rec_to_dis, null, null, null, 373, 373, '', '', 38, 38);
+            /*echo "<pre>";
+            print_r($records);
+            echo "</pre>";
+            exit();*/
+            $template_arry['tabRender'] = "home/tab_render.html";
+            $template_arry['articleRow'] = "home/article_row.html";
+            $template_arry['trackRow'] = "home/music_row.html";
 
-			/*$records = $this->browse_recommended->fetch_new_tracks("",$this->rec_to_dis,NULL,NULL,NULL,373,373,'','',38,38);	*/
-			$records = $this->home_modal->get_overview("",$this->rec_to_dis,NULL,NULL,NULL,373,373,'','',38,38);	
-			/*echo "<pre>";
-			print_r($records);
-			echo "</pre>";
-			exit();*/
-			$template_arry['tabRender']= "home/tab_render.html";		
-			$template_arry['articleRow'] = "home/article_row.html";
-			$template_arry['trackRow'] = "home/music_row.html";
+            $overview_active = true;
+            $this->config->set_item('title', 'Overview');
+        }
 
-			$overview_active = true;
-			$this->config->set_item('title','Overview');
-		}
+        //$notification = $this->home_modal->get_my_notification("",$this->rec_to_dis);
+        //dump($notification);
+        $data = [
+            'records' => $records,
+            'music_active' => ($music_active) ? $music_active : false,
+            'instrument_active' => ($instrument_active) ? $instrument_active : false,
+            'overview_active' => ($overview_active) ? $overview_active : false,
+            'license_active' => ($license_active) ? $license_active : false,
+            'all_news_active' => $all_news_active,
+            'home_page' => "true",
+            'loadmore' => $loadmore,
+            'load_more_url' => $load_url,
+            'template' => $load_tmpl,
+            'container' => $load_cont,
+        ];
+        $template_arry['MainPanel'] = "main.html";
+        $template_arry['leftPanel'] = "left_panel.html";
+        $template_arry['rightPanel'] = "right_panel.html";
+        //$template_arry['bigPlayerPanel']="big_player.html";
+        $template_arry['contentPanel'] = "headlines.html";
+        //$template_arry['notificationRow']="notification_row.html";
+        //$template_arry['newsRow']="news_row.html";
 
-		//$notification = $this->home_modal->get_my_notification("",$this->rec_to_dis);
-		//dump($notification);
-		$data = array(			
-			'records'=>$records,
-			'music_active' => ($music_active) ? $music_active : false,
-			'instrument_active' => ($instrument_active) ? $instrument_active : false,
-			'overview_active' => ($overview_active) ? $overview_active : false,
-			'license_active' => ($license_active) ? $license_active : false,			
-			'all_news_active' => $all_news_active,
-			'home_page' => "true",
-			'loadmore' => $loadmore,
-			'load_more_url' => $load_url,
-			'template' => $load_tmpl,
-			'container' => $load_cont
-			);
-		$template_arry['MainPanel'] = "main.html";
-		$template_arry['leftPanel'] = "left_panel.html";
-		$template_arry['rightPanel'] = "right_panel.html";
-		//$template_arry['bigPlayerPanel']="big_player.html";
-		$template_arry['contentPanel'] = "headlines.html";
-		//$template_arry['notificationRow']="notification_row.html";
-		//$template_arry['newsRow']="news_row.html";	
-
-		if(isset($temp_name) && $temp_name != null)	
-			$template_arry['recordsRow']=$temp_name;		
-		if($loadmore == "true")
-			$template_arry['loadMore'] = "general/loadmore.html";
+        if (isset($temp_name) && $temp_name != null) {
+            $template_arry['recordsRow'] = $temp_name;
+        }
+        if ($loadmore == "true") {
+            $template_arry['loadMore'] = "general/loadmore.html";
+        }
 
 
-		$template_arry['playerPanel']="player_panel.html";
-                //var_dump ($data);exit;
-		$data1=get_template_content($template_arry,$data);
-                //var_dump ($data1);exit;
-		$a['data'] = $data1;
-		$a['redirectURL']=base_url();
-		$this->load->view('home',$a);
-	}
-	
+        $template_arry['playerPanel'] = "player_panel.html";
+        //var_dump ($data);exit;
+        $data1 = get_template_content($template_arry, $data);
+        //var_dump ($data1);exit;
+        $a['data'] = $data1;
+        $a['redirectURL'] = base_url();
+        $this->load->view('home', $a);
+    }
+
 }
 
 /* End of file welcome.php */
--- application/models/home_modal.php
+++ application/models/home_modal.php
@@ -1,200 +1,220 @@
 <?php
+
 Class home_modal extends CI_Model
 {
-	function __construct()
-	{
+    function __construct()
+    {
         // Call the Model constructor
-		parent::__construct();
-		$this->load->model('commonfn');
-		$this->sess_uid = $this->session->userdata('user')->id;
-	}
-	
-	/*similar songs*/
-	function get_my_notification($cond = NULL,$limit = NULL,$orderby = NULL,$counter = NULL)
-	{
+        parent::__construct();
+        $this->load->model('commonfn');
+        $this->sess_uid = $this->session->userdata('user')->id;
+    }
+
+    /*similar songs*/
+    function get_my_notification($cond = null, $limit = null, $orderby = null, $counter = null)
+    {
 
 
-		$output = array();
+        $output = [];
 
-		if($cond != NULL)
-			$cond = " WHERE n.toId = '".$this->sess_uid."' AND u.id = n.fromId AND ".$cond."";	
-		else				
-			$cond = " WHERE n.toId = '".$this->sess_uid."' AND u.id = n.fromId ";
+        if ($cond != null) {
+            $cond = " WHERE n.toId = '" . $this->sess_uid . "' AND u.id = n.fromId AND " . $cond . "";
+        } else {
+            $cond = " WHERE n.toId = '" . $this->sess_uid . "' AND u.id = n.fromId ";
+        }
 
-		if($limit != NULL)
-			$limit = " LIMIT ".$limit." ";
+        if ($limit != null) {
+            $limit = " LIMIT " . $limit . " ";
+        }
 
-		if($orderby != NULL)
-			$orderby ="ORDER BY n.id DESC,".$orderby;	
-		else
-			$orderby ="ORDER BY n.id DESC";	
+        if ($orderby != null) {
+            $orderby = "ORDER BY n.id DESC," . $orderby;
+        } else {
+            $orderby = "ORDER BY n.id DESC";
+        }
 
-		$query = $this->db->query("SELECT n.id,n.notification,n.fromId,n.createdDate,u.firstname,u.lastname,u.profileLink FROM notification as n,users as u ".$cond." ".$orderby." ".$limit." ");
+        $query = $this->db->query("SELECT n.id,n.notification,n.fromId,n.createdDate,u.firstname,u.lastname,u.profileLink FROM notification as n,users as u " . $cond . " " . $orderby . " " . $limit . " ");
 
-		if($counter != NULL)
-			return $query->num_rows();	
+        if ($counter != null) {
+            return $query->num_rows();
+        }
 
-			//echo print_query();
-		foreach ($query->result_array() as $row)
-		{
-			$row["user_pic"] = $this->commonfn->get_photo('p',$row["fromId"]);
-			$row["timeago"] = timeago($row["createdDate"]);
+        //echo print_query();
+        foreach ($query->result_array() as $row) {
+            $row["user_pic"] = $this->commonfn->get_photo('p', $row["fromId"]);
+            $row["timeago"] = timeago($row["createdDate"]);
 
-			$output[] = $row;			
-		}
-		return $output;
-	}
-	/*similar songs ends*/
+            $output[] = $row;
+        }
 
-	/*Function for fetching new tracks*/
-	function get_overview($condition = NULL,$limit = NULL,$orderby = NULL,$counter = NULL,$start_limit = NULL,$width = NULL,$height = NULL,$images_only = NULL,$userId = NULL,$uwidth = NULL,$uheight = NULL)
-	{
-		$userId = ($userId != NULL) ? $userId : $this->sess_uid;
-		$this->load->model('commonfn');
-		$output = array();
-		if($userId > 0)
-			$cond = " tt.status = 'y' AND ((tt.isPublic='n' and tt.userId='".$userId."') or tt.isPublic='y')";	
-		else
-			$cond = " tt.status = 'y' AND tt.isPublic='y'";
+        return $output;
+    }
+    /*similar songs ends*/
+
+    /*Function for fetching new tracks*/
+    function get_overview($condition = null,
+                          $limit = null,
+                          $orderby = null,
+                          $counter = null,
+                          $start_limit = null,
+                          $width = null,
+                          $height = null,
+                          $images_only = null,
+                          $userId = null,
+                          $uwidth = null,
+                          $uheight = null)
+    {
+        $userId = ($userId != null) ? $userId : $this->sess_uid;
+        $this->load->model('commonfn');
+        $output = [];
+        if ($userId > 0) {
+            $cond = " tt.status = 'y' AND ((tt.isPublic='n' and tt.userId='" . $userId . "') or tt.isPublic='y')";
+        } else {
+            $cond = " tt.status = 'y' AND tt.isPublic='y'";
+        }
 
 
-		if($condition != NULL)
-			$cond .= " AND".$condition;
+        if ($condition != null) {
+            $cond .= " AND" . $condition;
+        }
 
-		if($orderby != NULL)
-			$orderby ="ORDER BY tt.id DESC,".$orderby;	
-		else
-			$orderby ="ORDER BY tt.id DESC";	
+        if ($orderby != null) {
+            $orderby = "ORDER BY tt.id DESC," . $orderby;
+        } else {
+            $orderby = "ORDER BY tt.id DESC";
+        }
 
-		$this->db->select('tt.id,tt.featured,tt.title,tt.release_mm,tt.release_dd,tt.release_yy,tt.createdDate,tt.trackuploadbpm,tt.timelength,tt.plays,tt.comments,tt.shares,tt.perLink,tt.isPublic,tt.userId as trackuserid,g.genre,u.firstname as artist_name,u.lastname,u.id as uid,u.profileLink,a.name as album');
-		$this->db->from('tracks as tt');
-		$this->db->join('genre as g', 'tt.genreId = g.id','left');
-		$this->db->join('albums as a', 'tt.albumId = a.id','left');
-		$this->db->join('users as u', 'tt.userId = u.id','right');
+        $this->db->select('tt.id,tt.featured,tt.title,tt.release_mm,tt.release_dd,tt.release_yy,tt.createdDate,tt.trackuploadbpm,tt.timelength,tt.plays,tt.comments,tt.shares,tt.perLink,tt.isPublic,tt.userId as trackuserid,g.genre,u.firstname as artist_name,u.lastname,u.id as uid,u.profileLink,a.name as album');
+        $this->db->from('tracks as tt');
+        $this->db->join('genre as g', 'tt.genreId = g.id', 'left');
+        $this->db->join('albums as a', 'tt.albumId = a.id', 'left');
+        $this->db->join('users as u', 'tt.userId = u.id', 'right');
 
-		if($start_limit != NULL && $limit != NULL)	
-		{
-			$this -> db -> limit($limit,$start_limit);	
-		}else{
-			if($limit != NULL)
-				$this -> db -> limit($limit);
-		}
+        if ($start_limit != null && $limit != null) {
+            $this->db->limit($limit, $start_limit);
+        } else {
+            if ($limit != null) {
+                $this->db->limit($limit);
+            }
+        }
 
-		$this->db->where($cond);
-		$this->db->order_by("tt.id","desc");			
-		$query = $this->db->get();
-                //var_dump($query);exit;
-		$total_counter = $query->num_rows();
+        $this->db->where($cond);
+        $this->db->order_by("tt.id", "desc");
+        $query = $this->db->get();
+        //var_dump($query);exit;
+        $total_counter = $query->num_rows();
 
-		if($counter != NULL)
-			return $query->num_rows();
-		$i = 1;
-                                                
-		if($start_limit != NULL)
-			$i = $start_limit+1;
-                $query2 = $this->db->query("SELECT id,username from users");
-                $usernames_id = array();                                
-                $usernames = array();                                
-                foreach ($query2->result_array() as $row2){  
-                    $new_array = array($row2['id'] => $row2['username']);                    
-                    $usernames =  array_merge($usernames, $new_array);                    
-                    array_push($usernames_id, $row2['id']);
+        if ($counter != null) {
+            return $query->num_rows();
+        }
+        $i = 1;
+
+        if ($start_limit != null) {
+            $i = $start_limit + 1;
+        }
+        $query2 = $this->db->query("SELECT id,username from users");
+        $usernames_id = [];
+        $usernames = [];
+        foreach ($query2->result_array() as $row2) {
+            $new_array = [$row2['id'] => $row2['username']];
+            $usernames = array_merge($usernames, $new_array);
+            array_push($usernames_id, $row2['id']);
+        }
+
+        for ($i = 0; $i < count($usernames); $i++) {
+            if (!isset($usernames[$usernames_id[$i]]) && $usernames[$i]) {
+                $usernames[$usernames_id[$i]] = $usernames[$i];
+            }
+            //var_dump ($usernames[$i]);
+        }
+        //var_dump( $usernames);exit;
+        //load db for home page
+        foreach ($query->result_array() as $row) {
+            //var_dump($row);exit;
+
+            $row["track_image"] = $this->commonfn->get_photo('t', $row["id"], $width, $height);
+            $row["user_image"] = $this->commonfn->get_photo('p', $row["uid"], $uwidth, $uheight);
+            //$row["main_title"] = character_limiter($row["title"], 40,$end_char = '&#8230;');
+            $row["main_title"] = character_limiter($row["title"], 35, $end_char = '');
+
+            $row["artist_mini_name"] = character_limiter($row["artist_name"], 20, $end_char = '&#8230;');
+            $row["i"] = $i;
+            $row["total_songs"] = "";
+            $row["featured"] = ($row["featured"] == 'y' ? true : false);
+            //$row["waveform"] = img_url()."wave1.png";
+            $row["waveform"] = $row['waveform'];
+            //$row["wave"] = img_url() . 'hover-wave-img.png';
+            $row["trackLink"] = base_url() . $row["profileLink"] . "/" . $row["perLink"];
+            $row["profileLink"] = base_url() . $row["profileLink"];
+            $row["album"] = ($row["album"] == "" || $row["album"] == null) ? "-" : $row["album"];
+            $row["is_track"] = true;
+
+            $row["like_js_class"] = "like_js";
+            $row["like_class"] = "like-icon";
+
+            $row["bpm"] = $row["trackuploadbpm"];
+            $row["username"] = $usernames[$row["uid"]];
+            //var_dump ($row);exit;
+
+
+            if ($userId > 0) {
+                $is_liked = getvalfromtbl("id", "likelog", " userId = '" . $userId . "'  AND trackId = '" . $row["id"] . "'", "single", "");
+                if ($is_liked > 0) {
+                    $row["like_js_class"] = "dislike_js";
+                    $row["like_class"] = "unlike-icon";
                 }
-                
-                for($i=0;$i< count($usernames); $i++){
-                    if (! isset($usernames[$usernames_id[$i]]) && $usernames[$i])
-                    $usernames[$usernames_id[$i]] = $usernames[$i];
-                    //var_dump ($usernames[$i]);
-                }
-                //var_dump( $usernames);exit;
-                //load db for home page
-		foreach ($query->result_array() as $row)
-		{   
-                        //var_dump($row);exit;
-                        
-			$row["track_image"] = $this->commonfn->get_photo('t',$row["id"],$width,$height);
-			$row["user_image"] = $this->commonfn->get_photo('p',$row["uid"],$uwidth,$uheight);
-			//$row["main_title"] = character_limiter($row["title"], 40,$end_char = '&#8230;');
-                        $row["main_title"] = character_limiter($row["title"], 35,$end_char = '');
-                        
-			$row["artist_mini_name"] = character_limiter($row["artist_name"], 20,$end_char = '&#8230;');		
-			$row["i"] = $i;
-			$row["total_songs"] = "";
-			$row["featured"]= ($row["featured"]=='y'?true:false);
-				//$row["waveform"] = img_url()."wave1.png";
-			$row["waveform"] = base_url()."waveform/".$row["profileLink"]."/".$row["perLink"].".json";
-			$row["wave"] = img_url().'hover-wave-img.png';
-			$row["trackLink"] = base_url().$row["profileLink"]."/".$row["perLink"];
-			$row["profileLink"] = base_url().$row["profileLink"];				
-			$row["album"] = ($row["album"] == "" || $row["album"] == NULL) ? "-" : $row["album"]; 
-			$row["is_track"] = true;
-                        
-			$row["like_js_class"] = "like_js";
-			$row["like_class"] = "like-icon";
-                        
-                        $row["bpm"] = $row["trackuploadbpm"];
-			$row["username"] = $usernames[$row["uid"]];
-			//var_dump ($row);exit;
-                        
-                        
-			if($userId > 0){
-				$is_liked = getvalfromtbl("id","likelog"," userId = '".$userId."'  AND trackId = '".$row["id"]."'","single","");
-				if($is_liked > 0){
-					$row["like_js_class"] = "dislike_js";	
-					$row["like_class"] = "unlike-icon";
-				}				
-			}
-			$output[] = $row;
-			$i++;				
-		}
-                //exit;
+            }
+            $output[] = $row;
+            $i++;
+        }
+        //exit;
 
-		
 
-		/*Fetch Articles*/
-		$cond2 = "a.status = 'y' AND a.headlineDisp = 'y'";
-		$this->db->select('a.*');
-		$this->db->from('articles as a');
-		$this->db->where($cond2);
-		$this->db->order_by("a.id","desc");		
-		if($start_limit != NULL && $limit != NULL)	
-		{
-			$this -> db -> limit($limit,$start_limit);	
-		}else{
-			if($limit != NULL)
-				$this -> db -> limit($limit);
-		}	
-		$query2 = $this->db->get();
+        /*Fetch Articles*/
+        $cond2 = "a.status = 'y' AND a.headlineDisp = 'y'";
+        $this->db->select('a.*');
+        $this->db->from('articles as a');
+        $this->db->where($cond2);
+        $this->db->order_by("a.id", "desc");
+        if ($start_limit != null && $limit != null) {
+            $this->db->limit($limit, $start_limit);
+        } else {
+            if ($limit != null) {
+                $this->db->limit($limit);
+            }
+        }
+        $query2 = $this->db->get();
 
-		$total_counter += $query2->num_rows();
+        $total_counter += $query2->num_rows();
 
-		if($counter != NULL)
-			return $query2->num_rows();
-		$i = 1;
-		if($start_limit != NULL)
-			$i = $start_limit+1;
+        if ($counter != null) {
+            return $query2->num_rows();
+        }
+        $i = 1;
+        if ($start_limit != null) {
+            $i = $start_limit + 1;
+        }
 
-		foreach ($query2->result_array() as $row)
-		{
-			$row["article_image"] = $this->commonfn->get_photo('art',$row["id"],371,371);	
-			$row["is_article"] = true;
-			$output[] = $row;
-		}
-		/*Fetch Articles ends*/
-		$ord = array();
-		foreach ($output as $key => $value){
-			$ord[] = strtotime($value['createdDate']);
-		}
-		array_multisort($ord, SORT_DESC, $output);
-		/*echo "<pre>";
-		print_r($output);
-		exit();*/
-		if($limit > $total_counter)
-		{
-			/*call again this function*/
-		}
-		return $output;
-	}
+        foreach ($query2->result_array() as $row) {
+            $row["article_image"] = $this->commonfn->get_photo('art', $row["id"], 371, 371);
+            $row["is_article"] = true;
+            $output[] = $row;
+        }
+        /*Fetch Articles ends*/
+        $ord = [];
+        foreach ($output as $key => $value) {
+            $ord[] = strtotime($value['createdDate']);
+        }
+        array_multisort($ord, SORT_DESC, $output);
+        /*echo "<pre>";
+        print_r($output);
+        exit();*/
+        if ($limit > $total_counter) {
+            /*call again this function*/
+        }
+
+        return $output;
+    }
 
 }//modal over
 ?>
