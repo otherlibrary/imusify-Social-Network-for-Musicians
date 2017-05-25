78d3e9d9226599d9521983c55b26451861e1b80c
solomko2 <solomko2@yandex.ru>
2017-05-25 18:35:16 +0300

    playlist view mocks

--- frontend/src/app/components/playlist/playlist.component.ts
+++ frontend/src/app/components/playlist/playlist.component.ts
@@ -1,7 +1,7 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {Component, NgZone, OnInit, ViewChild} from '@angular/core';
 import {Observable} from "rxjs";
 import {Router} from "@angular/router";
-import {Playlist} from "../../interfases/playlist/Playlist";
+import {IPlaylists, Playlist} from "../../interfases/playlist/Playlist";
 import {SwiperConfigInterface, SwiperDirective} from "ngx-swiper-wrapper";
 
 @Component({
@@ -11,6 +11,10 @@ import {SwiperConfigInterface, SwiperDirective} from "ngx-swiper-wrapper";
 })
 export class PlaylistComponent implements OnInit {
   public isShow: boolean = false;
+  public playlistData: IPlaylists;
+  public playlist: Playlist[];
+  public currentPlaylist: Playlist;
+  public current: number;
 
   public swiperConfig: SwiperConfigInterface = {
     slidesPerView: 2,
@@ -19,26 +23,49 @@ export class PlaylistComponent implements OnInit {
     initialSlide: 1,
     nextButton: '.wrap-playlist .swiper-button-next',
     prevButton: '.wrap-playlist .swiper-button-prev',
+    onSlideChangeEnd: (slider) => {
+      this.zone.run(() => {
+        this.current = slider.activeIndex;
+        this.currentPlaylist = this.playlist[this.current - 1];
+        console.log(this.currentPlaylist);
+      });
+    }
   };
+
   @ViewChild(SwiperDirective) swiperView: SwiperDirective;
 
-  constructor(private _router: Router) {
+  constructor(private _router: Router,
+              public zone: NgZone) {
   }
 
   ngOnInit() {
     this.getTracks();
+
   }
 
   getTracks() {
-    this.server().subscribe(() => {
+    this.server().subscribe((res: IPlaylists) => {
+      this.playlistData = res;
+      this.playlist = res.playlist;
       this.isShow = true;
     })
   }
 
   server() {
+    let playlistData = {
+      currentId: '2',
+      loggedin: true,
+      playlist: []
+    };
+    for (let i = 0; i < 10; i++) {
+      playlistData.playlist.push(new Playlist(
+        '' + i, '10' + i, '20.10.2010', '212'+i, 'cool play list ' + i, '/assets/images/profile/vinil-face1.jpg', '21', '223', 'y', '10.12.2016',
+        '2', '2121', '21'
+      ));
+    }
     return new Observable(observable => {
       setTimeout(function () {
-        observable.next(123)
+        observable.next(playlistData)
       }, 500);
     })
   }
@@ -46,5 +73,13 @@ export class PlaylistComponent implements OnInit {
   closePlaylist() {
     this.isShow = false;
     this._router.navigate([{outlets: {popup: null}}]);
+  }
+
+  goToSlideIndex(index) {
+    this.swiperView.swiper.slideTo(index);
+  }
+
+  onIndexChange(e) {
+    console.log(32323232);
   }
 }
